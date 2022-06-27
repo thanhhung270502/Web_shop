8504284c3a3340bab772b7a0ce6b9f2d7bdb239b
thanhhung270502 <thanhhung270502@gmail.com>
2022-06-27 20:11:37 +0700

    Header #4 - Menu + Button

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,70 +1,77 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 import Tippy from '@tippyjs/react/headless';
 
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 // import images from '~/assets/images';
-import AccountItem from '~/components/AccountItem';
+// import AccountItem from '~/components/AccountItem';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
 
-function Header() {
-    const [menuResult, setMenuResult] = useState([]);
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faSortDown} />,
+        title: 'Áo',
+        to: '/ao',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faSortDown} />,
+        title: 'Quần',
+        to: '/quan',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faSortDown} />,
+        title: 'Phụ kiện',
+        to: '/phukien',
+    },
+];
 
-    useEffect(() => {
-        setTimeout(() => {
-            setMenuResult([1, 2, 3]);
-        }, 0);
-    }, []);
+function Header() {
+    // const [menuResult, setMenuResult] = useState([]);
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setMenuResult([1, 2, 3]);
+    //     }, 0);
+    // }, []);
     return (
         <header className={cx('wrapper')}>
             <div className={cx('logo')}>
                 <div className={cx('inner-logo')}>
-                    <span>Huyen</span>
-                    Trinh
+                    <span>L</span>O<span>G</span>O
                 </div>
             </div>
-            <Tippy
-                interactive="true"
-                visible={menuResult.length > 0}
-                render={(attrs) => (
-                    <div className={cx('menu-result')} tabIndex="-1" {...attrs}>
-                        <PopperWrapper>
-                            <h4 className={cx('menu-title')}>Accounts</h4>
-                            <AccountItem />
-                            <AccountItem />
-                            <AccountItem />
-                            <AccountItem />
-                        </PopperWrapper>
-                    </div>
-                )}
-            >
-                <div className={cx('menu')}>
-                    <div className={cx('li')}>
-                        <a href="/" className={cx('active')}>
-                            Home
+
+            <div className={cx('menu')}>
+                <div className={cx('li')}>
+                    <a href="/" className={cx('active')}>
+                        Home
+                    </a>
+                </div>
+                <Menu items={MENU_ITEMS}>
+                    <div className={cx('menu-place')}>
+                        <a href="/">
+                            Shop
+                            <FontAwesomeIcon className={cx('icon-down')} icon={faAngleDown} />
                         </a>
                     </div>
+                </Menu>
 
-                    <div className={cx('menu-place')}>
-                        <a href="/">Shop</a>
-                    </div>
-
-                    <div className={cx('li')}>
-                        <a href="/">Blog</a>
-                    </div>
-                    <div className={cx('li')}>
-                        <a href="/">Contact</a>
-                    </div>
-                    <div className={cx('li')}>
-                        <a href="/">Log in</a>
-                    </div>
+                <div className={cx('li')}>
+                    <a href="/">Blog</a>
                 </div>
-            </Tippy>
+                <div className={cx('li')}>
+                    <a href="/">Contact</a>
+                </div>
+                <div className={cx('li')}>
+                    <a href="/">Log in</a>
+                </div>
+            </div>
         </header>
     );
 }
